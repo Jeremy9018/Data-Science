9999f3e1e22357642ce494f36522a8c3476e8240
Jeremy9018 <55352546+Jeremy9018@users.noreply.github.com>
2022-08-08 14:33:38 +0900

    Update Cross Category From GA.py

--- Cross Category From GA.py
+++ Cross Category From GA.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 import os
 
-
 ## Variable 
 input_filename = 'final_raw_de.csv'
 category_raw = 'Web - Page Category2(149)'
@@ -13,7 +12,7 @@ include_word = ['de:']
 remove_word = ['brand','offer','CATEGORY','about','cs:','2021',':business:','blog','magazine','cloud','test1','2020','2019','career','microsite','promotion','Charity']
 output_name = 'output.csv'
 
-###############################################################################
+##########################
 
 input_directory = 'C:/Users/{}/Downloads/{}'.format(os.getlogin(),input_filename)
 
@@ -32,21 +31,15 @@ def filter_df(df,column,word_array,include=False):
             df_output.drop_duplicates(inplace=True)
     return df_output
 
-#df = df_raw[df_raw['Web - Page Category2(149)'].str.contains(country + ':')]
-
 df = filter_df(df_raw,category_raw,include_word,include=True) #Filter for Country
 df = filter_df(df,category_raw,remove_word1,include=False) #Filter out Non product page
 
 # 체크용 df['Web - Page Category2(149)'].unique()
 
 df['Category'] = df.loc[:,category_raw].apply(lambda x:x.split(':')[cat_location-1].lower() if len(x.split(':')) >= cat_location else NULL) #Getting Category data from category_raw column
-
 df = df[df['Category'] != 0] #None 0 Category
-
 df_filtered = df.loc[:,['Web - Client ID(102)','Category']]
-
 df_join = pd.merge(df_filtered,df_filtered,on=['Web - Client ID(102)'])
-
 
 df_output = df_join.groupby(['Category_x','Category_y'],as_index=False).nunique() #Count unique CID
 df_output= df_output[df_output['Category_x'] <= df_output['Category_y']]
